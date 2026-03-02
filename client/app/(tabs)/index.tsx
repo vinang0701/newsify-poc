c4167ca1f8e6df3b591c61e51a22f8909b6c7fc0
vinang0701 <vinang0701@gmail.com>
2026-03-02 19:05:52 +0800

    added category filters

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -8,201 +8,302 @@ import { Link } from "expo-router";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import {
-    ScrollView,
-    StyleSheet,
-    TouchableHighlight,
-    useColorScheme,
-    View,
+	Text,
+	Button,
+	FlatList,
+	Pressable,
+	ScrollView,
+	StyleSheet,
+	TouchableHighlight,
+	useColorScheme,
+	View,
 } from "react-native";
 
 const HEADER_HEIGHT = 250;
 
 type News = {
-    author: string;
-    title: string;
-    desc: string;
-    url: string;
-    urlToImage: string;
-    content: string;
+	title: string;
+	author: string;
+	desc: string;
+	url: string;
+	urlToImage: string;
+	content: string;
 };
+
+const DATA = [
+	{
+		id: "1",
+		title: "Recent",
+	},
+	{
+		id: "2",
+		title: "Important",
+	},
+	{
+		id: "3",
+		title: "Following",
+	},
+];
 
 // Substack like news feed
 // avatar, name, time ago, 3 dots on the right
 // title, image, preview text, read more
 // like, comment, repost, save
 
+type ItemProps = { title: string };
+const FilterItem = ({ title }: ItemProps) => {
+	const colorScheme = useColorScheme() ?? "light";
+
+	return (
+		<Pressable
+			style={{
+				paddingHorizontal: 12,
+				paddingVertical: 4,
+				backgroundColor: Colors[colorScheme].bg_light,
+				borderRadius: 4,
+				marginRight: 8,
+			}}
+		>
+			<Text
+				style={{
+					color: Colors[colorScheme].tint,
+				}}
+			>
+				{title}
+			</Text>
+		</Pressable>
+	);
+};
+
 export default function HomeScreen() {
-    const colorScheme = useColorScheme() ?? "light";
-    const [news, setNews] = useState<News[]>([]);
-    useEffect(() => {
-        const fetchNews = () => {
-            // try {
-            //     const response = await fetch(
-            //         "https://newsapi.org/v2/top-headlines?country=us&apiKey=3d96b466effb4a18aaf81eb8c202feda",
-            //         {
-            //             method: "GET",
-            //             headers: {
-            //                 Accept: "application/json",
-            //                 "Content-Type": "application/json",
-            //             },
-            //         },
-            //     );
+	const colorScheme = useColorScheme() ?? "light";
 
-            //     const json = await response.json();
-            //     setNews(json.articles);
-            // } catch (error) {
-            //     console.error(error);
-            // }
-            newsArticles.articles.forEach((newsItem) => {
-                news.push({
-                    title: newsItem.title,
-                    author: newsItem.author ?? "",
-                    desc: newsItem.description ?? "",
-                    url: newsItem.url ?? "",
-                    urlToImage: newsItem.urlToImage ?? "",
-                    content: newsItem.content ?? "",
-                });
-            });
-        };
-        fetchNews();
-    }, [news]);
-    return (
-        <ScrollView>
-            {news.map((newsItem, index) => {
-                return (
-                    <View
-                        key={index}
-                        style={[
-                            styles.card,
-                            {
-                                backgroundColor: Colors[colorScheme].bg_dark,
-                            },
-                        ]}
-                    >
-                        <View style={styles.cardInfoContainer}>
-                            <Image
-                                source={require("@/assets/images/profile.png")}
-                                style={{ width: 28, height: 28 }}
-                            />
-                            <ThemedText type="defaultSemiBold">
-                                Author
-                            </ThemedText>
-                            <ThemedText
-                                type="default"
-                                style={{
-                                    fontSize: 10,
-                                    color: "hsl(0, 0%, 5%)",
-                                }}
-                            >
-                                1d
-                            </ThemedText>
-                            <TouchableHighlight style={{ marginLeft: "auto" }}>
-                                <Feather
-                                    name="more-vertical"
-                                    size={20}
-                                    color={Colors[colorScheme].icon}
-                                />
-                            </TouchableHighlight>
-                        </View>
-                        <View>
-                            {/* Content */}
-                            <Image
-                                alt="image"
-                                source={{
-                                    uri: newsItem.urlToImage,
-                                }}
-                                style={{
-                                    width: "100%",
-                                    height: 200,
-                                    resizeMode: "cover",
-                                }}
-                            />
-                            <ThemedText
-                                type="title"
-                                style={{
-                                    paddingTop: 12,
-                                    paddingHorizontal: 12,
-                                    fontSize: 20,
-                                }}
-                            >
-                                {newsItem.title}
-                            </ThemedText>
-                            <ThemedText
-                                style={{
-                                    paddingVertical: 4,
-                                    paddingHorizontal: 12,
-                                    fontSize: 14,
-                                }}
-                            >
-                                {newsItem.content?.replace(
-                                    /\s*\[\+\d+ chars\]$/,
-                                    "",
-                                )}
-                                <Link href="/(tabs)/create-post">
-                                    <ThemedText
-                                        type="link"
-                                        style={{ fontSize: 14 }}
-                                    >
-                                        Read More
-                                    </ThemedText>
-                                </Link>
-                            </ThemedText>
-                        </View>
+	const [news, setNews] = useState<News[]>([]);
+	// const articles = newsArticles.articles.map((newsItem: News) => {
+	// 	title: newsItem.title;
+	// 	author: newsItem.author ?? "";
+	// 	desc: newsItem.desc ?? "";
+	// 	url: newsItem.url ?? "";
+	// 	urlToImage: newsItem.urlToImage ?? "";
+	// 	content: newsItem.content ?? "";
+	// });
+	useEffect(() => {
+		const articles = newsArticles.articles.map((newsItem, index) => ({
+			title: newsItem.title,
+			author: newsItem.author ?? "",
+			desc: newsItem.description ?? "",
+			url: newsItem.url ?? "",
+			urlToImage: newsItem.urlToImage ?? "",
+			content: newsItem.content ?? "",
+		}));
 
-                        <View style={styles.iconsContainer}>
-                            {/* Interaction */}
-                            <Feather name="heart" size={24} color="black" />
-                            <SimpleLineIcons
-                                name="bubble"
-                                size={24}
-                                color="black"
-                            />
-                            <Feather name="repeat" size={24} color="black" />
-                            <Feather name="bookmark" size={24} color="black" />
-                        </View>
-                    </View>
-                );
-            })}
-        </ScrollView>
-    );
+		setNews((prev) => [...prev, ...articles]);
+	}, []);
+
+	return (
+		<ScrollView
+			style={[
+				styles.bodyContainer,
+				{ backgroundColor: Colors[colorScheme].bg },
+			]}
+		>
+			<View style={{ marginBottom: 12 }}>
+				<FlatList
+					data={DATA}
+					renderItem={({ item }) => <FilterItem title={item.title} />}
+					keyExtractor={(item) => item.id}
+					horizontal={true}
+				/>
+			</View>
+
+			{news.map((newsItem, index) => {
+				return (
+					<View
+						key={index}
+						style={[
+							styles.card,
+							{
+								backgroundColor: Colors[colorScheme].bg_light,
+							},
+						]}
+					>
+						<View style={styles.cardInfoContainer}>
+							<Image
+								source={require("@/assets/images/profile.png")}
+								style={{ width: 28, height: 28 }}
+							/>
+							<ThemedText type="defaultSemiBold">
+								Author
+							</ThemedText>
+							<ThemedText
+								type="default"
+								style={{
+									fontSize: 10,
+									color: "hsl(0, 0%, 5%)",
+								}}
+							>
+								1d
+							</ThemedText>
+							<TouchableHighlight style={{ marginLeft: "auto" }}>
+								<Feather
+									name="more-vertical"
+									size={20}
+									color={Colors[colorScheme].icon}
+								/>
+							</TouchableHighlight>
+						</View>
+						<View>
+							{/* Content */}
+							<Image
+								alt="image"
+								source={{
+									uri: newsItem.urlToImage,
+								}}
+								style={{
+									width: "100%",
+									height: 200,
+									resizeMode: "cover",
+								}}
+							/>
+							<ThemedText
+								type="title"
+								style={{
+									paddingTop: 12,
+									paddingHorizontal: 12,
+									fontSize: 20,
+								}}
+							>
+								{newsItem.title}
+							</ThemedText>
+							<ThemedText
+								style={{
+									paddingVertical: 4,
+									paddingHorizontal: 12,
+									fontSize: 14,
+								}}
+							>
+								{newsItem.content?.replace(
+									/\s*\[\+\d+ chars\]$/,
+									"",
+								)}
+								<Link href="/(tabs)/create-post">
+									<ThemedText
+										type="link"
+										style={{ fontSize: 14 }}
+									>
+										Read More
+									</ThemedText>
+								</Link>
+							</ThemedText>
+						</View>
+
+						<View style={styles.iconsContainer}>
+							{/* Interaction */}
+							<View
+								style={{
+									flex: 1,
+									flexDirection: "row",
+									justifyContent: "flex-start",
+								}}
+							>
+								<Pressable
+									style={{
+										flex: 1,
+										flexDirection: "row",
+										gap: 4,
+										alignItems: "center",
+										justifyContent: "flex-start",
+									}}
+								>
+									<Feather
+										name="heart"
+										size={24}
+										color="black"
+									/>
+									<Text>100</Text>
+								</Pressable>
+								<Pressable
+									style={{
+										flex: 1,
+										flexDirection: "row",
+										gap: 4,
+										justifyContent: "flex-start",
+										alignItems: "center",
+									}}
+								>
+									<Feather
+										name="message-square"
+										size={24}
+										color="black"
+									/>
+									<Text>100</Text>
+								</Pressable>
+								<Pressable
+									style={{
+										flex: 1,
+										flexDirection: "row",
+										justifyContent: "flex-start",
+										gap: 4,
+										alignItems: "center",
+									}}
+								>
+									<Feather
+										name="repeat"
+										size={24}
+										color="black"
+									/>
+									<Text>100</Text>
+								</Pressable>
+							</View>
+							<Feather name="bookmark" size={24} color="black" />
+						</View>
+					</View>
+				);
+			})}
+		</ScrollView>
+	);
 }
 
 const styles = StyleSheet.create({
-    header: {
-        height: HEADER_HEIGHT,
-        overflow: "hidden",
-    },
-    card: {
-        flex: 1,
-        gap: 8,
-        alignContent: "flex-start",
-        borderRadius: 8,
-        paddingVertical: 24,
-        marginBottom: 4,
-        minHeight: 200,
-    },
-    cardInfoContainer: {
-        flex: 1,
-        gap: 8,
-        alignItems: "center",
-        flexDirection: "row",
-        paddingHorizontal: 12,
-    },
+	header: {
+		height: HEADER_HEIGHT,
+		overflow: "hidden",
+	},
+	bodyContainer: {
+		paddingHorizontal: 16,
+		paddingTop: 8,
+	},
+	card: {
+		flex: 1,
+		gap: 8,
+		alignContent: "flex-start",
+		borderRadius: 8,
+		paddingVertical: 24,
+		marginBottom: 4,
+		minHeight: 200,
+	},
+	cardInfoContainer: {
+		flex: 1,
+		gap: 8,
+		alignItems: "center",
+		flexDirection: "row",
+		paddingHorizontal: 12,
+	},
 
-    titleContainer: {
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 8,
-    },
-    stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-    },
-    iconsContainer: {
-        flex: 1,
-        flexDirection: "row",
-        justifyContent: "space-between",
-        paddingTop: 8,
-        paddingHorizontal: 12,
-    },
+	titleContainer: {
+		flexDirection: "row",
+		alignItems: "center",
+		gap: 8,
+	},
+	stepContainer: {
+		gap: 8,
+		marginBottom: 8,
+	},
+	iconsContainer: {
+		flex: 1,
+		flexDirection: "row",
+		alignItems: "center",
+		paddingTop: 8,
+		paddingHorizontal: 12,
+	},
 });
